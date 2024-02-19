aae6ee8bfffc71b62b39dd8d7c0cb221e77d515d
Diego Torres <126494563+diegot-code@users.noreply.github.com>
2024-02-18 21:32:33 -0500

    second commit
    I learned to "return" format in functions/methods instead of "print"ing.

--- oop.py
+++ oop.py
@@ -6,6 +6,7 @@ name = "Shane"
 
 # print(type(name))
 
+x = 17
 
 class ThisClass:
     x = 5
@@ -13,29 +14,49 @@ class ThisClass:
 # print(ThisClass)
 
 # print(type(ThisClass))
+    
+# print(x)
 
 # print(ThisClass.x)
 
 #_____________________________________________________________________
 #  
 
-class Grocery:
+class Customer:
 
-    def __init__(self, category, quantity):
-        self.category = category
-        self.quantity = quantity
+    def __init__(self, name, budget):
+        self.name = name
+        self.budget = budget
 
+    # def __str__(self):
+    #     return f"{self.name} is looking for a car and has a budget of {self.budget}."
+        
+person1 = Customer("Denzel Washington", "40,000")
 
-
-
-
+# print(person1)
 
 #_____________________________________________________________________
 # 
 
+class Employee:
 
+    def __init__(self, name, empId, sme): # employee name, employee Id, Subject Matter Expert
+            self.name = name
+            self.empId = empId
+            self.sme = sme
 
+    def __str__(self):
+        return f"{self.name} is located in the Diagnosis Dep. He specialises in {self.sme} for Vehicles. If you wish to leave a review, you may reference his Id: {self.empId}."
+    
+    def available(self):
+        return f"{self.name} is available."
+    
+    def unavailable(self):
+        return f"{self.name} is unavailable at this time."
+    
+emp1 = Employee("Noah Williams", "7652-109-834", "Error Code Analysis")
 
+print(emp1.available())
 
 #_____________________________________________________________________
 # 
@@ -63,3 +84,10 @@ class Car:
     def slow(self):
         print("The"+ self.model +"is slowing down.")
 
+car1 = Car("2007", "BMW", "335i")
+
+car2 = Car("2023", "Chevy", "Corvette")
+
+# print(car1.make)
+
+# print(car2.make)
